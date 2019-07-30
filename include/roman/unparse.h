2670a11e450abb621a9f4bdf9d0107d9801637da
John Todd <j.todd@accenture.com>
2019-07-30 15:56:17 -0400

    refactor

--- include/roman/unparse.h
+++ include/roman/unparse.h
@@ -1,6 +1,6 @@
 #ifndef UNPARSE_H
 #define UNPARSE_H
 
-char *unparse_roman(const unsigned int);
+char *unparse_roman(unsigned int);
 
 #endif // UNPARSE_H
--- src/unparse.c
+++ src/unparse.c
@@ -3,10 +3,29 @@
 #include <string.h>
 #include "roman/unparse.h"
 
-#define MAX_VALUE 3999
+// Longest possible Roman number:
+// MMMDMCCCLCXXXVIII
+// which is 17 characters
+#define MAX_LENGTH 17
+#define MAX_VALUE  3999
 
-char *unparse_roman(const unsigned int arabic)
+static unsigned int num_roman_characters = 7;
+static char *roman_characters[] = {"M", "D", "C", "L", "X", "V", "I"};
+static unsigned int roman_character_values[] = {1000, 500, 100, 50, 10, 5, 1};
+
+static void append_chars_while_current_greater_than_value(unsigned int *current, const int value, char **ptr, const char *text)
 {
+    size_t text_len = strlen(text);
+    while (*current >= value) {
+        strcpy(*ptr, text);
+        *ptr += text_len;
+        *current -= value;
+    }
+}
+
+char *unparse_roman(unsigned int arabic)
+{
+    char buffer[MAX_LENGTH + 1];
     char *result;
 
     if (arabic > MAX_VALUE) {
@@ -16,22 +35,14 @@ char *unparse_roman(const unsigned int arabic)
         result = malloc(sizeof(char));
         result[0] = '\0';
     } else {
-        result = malloc(sizeof(char) * 2);
-        if (arabic == 1000) {
-            strcpy(result, "M");
-        } else if (arabic == 500) {
-            strcpy(result, "D");
-        } else if (arabic == 100) {
-            strcpy(result, "C");
-        } else if (arabic == 50) {
-            strcpy(result, "L");
-        } else if (arabic == 10) {
-            strcpy(result, "X");
-        } else if (arabic == 5) {
-            strcpy(result, "V");
-        } else if (arabic == 1) {
-            strcpy(result, "I");
+        char *ptr = buffer;
+        unsigned int current = arabic;
+        for (int i = 0; i < num_roman_characters; ++i) {
+            append_chars_while_current_greater_than_value(&current, roman_character_values[i], &ptr, roman_characters[i]);
         }
+        *ptr = '\0';
+        result = malloc(strlen(buffer) + 1);
+        strcpy(result, buffer);
     }
 
     return result;
